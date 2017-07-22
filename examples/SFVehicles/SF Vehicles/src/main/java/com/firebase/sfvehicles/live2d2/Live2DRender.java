09da8339a1ae27474332d5c8e9458b3db7b94647
LAPTOP-LQE1UOME\User <rudy20407@gmail.com>
2017-07-23 00:24:36 +0800

    Symptom: 加入live2d Solution:N/A Reviewer: N/A Impact: (影響) Comment: （註解）

---
+++ examples/SFVehicles/SF Vehicles/src/main/java/com/firebase/sfvehicles/live2d2/Live2DRender.java
@@ -0,0 +1,41 @@
+package com.firebase.sfvehicles.live2d2;
+
+import android.opengl.GLSurfaceView;
+
+
+import com.firebase.sfvehicles.live2d2.live2d.MyL2DModel;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by LUTAS on 2017/1/3.
+ */
+
+public class Live2DRender implements GLSurfaceView.Renderer
+{
+    private MyL2DModel mModelManager;
+
+    public Live2DRender() {
+        //mModelManagers = new ArrayList<>();
+    }
+
+    public void setModel(MyL2DModel modelManager) {
+        mModelManager = modelManager;
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        mModelManager.onSurfaceCreated(gl);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        mModelManager.onSurfaceChanged(gl, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        mModelManager.onDrawFrame(gl);
+    }
+}
